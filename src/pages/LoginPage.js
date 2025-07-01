2964c3ec3c060ae7389b2acea230bd428378a744
tejgokani <tejmgokani@gmail.com>
2025-07-01 21:37:38 +0530

    Update LoginPage.js

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -74,28 +74,26 @@ const LoginPage = ({ role }) => {
     };
 
     const guestModeHandler = () => {
-        const password = "zxc"
+    const password = "zxc";
 
-        if (role === "Admin") {
-            const email = "yogendra@12"
-            const fields = { email, password }
-            setGuestLoader(true)
-            dispatch(loginUser(fields, role))
-        }
-        else if (role === "Student") {
-            const rollNum = "1"
-            const studentName = "Dipesh Awasthi"
-            const fields = { rollNum, studentName, password }
-            setGuestLoader(true)
-            dispatch(loginUser(fields, role))
-        }
-        else if (role === "Teacher") {
-            const email = "tony@12"
-            const fields = { email, password }
-            setGuestLoader(true)
-            dispatch(loginUser(fields, role))
-        }
+    if (role === "Admin") {
+        const email = "yogendra@12";
+        const fields = { email, password };
+        setGuestLoader(true);
+        dispatch(loginUser(fields, role));
+    } else if (role === "Student") {
+        const rollNum = "1";
+        const studentName = "Dipesh Awasthi";
+        const fields = { rollNum, studentName, password };
+        setGuestLoader(true);
+        dispatch(loginUser(fields, role));
+    } else if (role === "Teacher") {
+        const email = "tony@12";
+        const fields = { email, password };
+        setGuestLoader(true);
+        dispatch(loginUser(fields, role));
     }
+};
 
     useEffect(() => {
         if (status === 'success' || currentUser !== null) {
